17c07ad6a9eea97ddcb0ff659bb190b53251afed
Rahul Sheth <rahul@Rahuls-MacBook-Pro.local>
2020-12-15 23:08:10 -0800

    Updates

---
+++ precache-manifest.ce68a6d7bb314f877d38cd9b36f429bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c508b350fd633f34542992cda4d970",
+    "url": "/test-dir/index.html"
+  },
+  {
+    "revision": "fb2a7f7d5c8436f16e56",
+    "url": "/test-dir/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0e7cf724d9c5c288c34a",
+    "url": "/test-dir/static/js/2.9edc178a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-dir/static/js/2.9edc178a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb2a7f7d5c8436f16e56",
+    "url": "/test-dir/static/js/main.b7a96571.chunk.js"
+  },
+  {
+    "revision": "b8e1e563945192cdc379",
+    "url": "/test-dir/static/js/runtime-main.15209ef5.js"
+  }
+]);
